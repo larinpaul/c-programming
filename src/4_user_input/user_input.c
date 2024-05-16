318913906015e721be5a0e027d9e9a23453b521e
larinpaul <www.larinpavel@mail.ru>
2024-05-16 08:03:15 +0500

    User Input in C

---
+++ src/4_user_input/user_input.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main() {
+
+	// Take int input
+
+//	int age;
+//
+//	printf("Enter input value: ");
+//	scanf("%d", &age); // & denotes memory access
+//
+//	printf("Age = %d", age);
+
+
+	// Take double and character input:
+
+//	double number;
+//	char alphabet;
+//
+//	printf("Enter double input: ");
+//	scanf("%lf", &number);
+//
+//	printf("Enter character input: ");
+//	scanf("\n%c", &alphabet);
+//
+//	printf("Number: %lf", number);
+//	printf("\nCharacter: %c \n", alphabet);
+
+
+	// TAke multiple input
+	double number;
+	char alphabet;
+
+	printf("Enter input values: ");
+	scanf("%lf %c", &number, &alphabet);
+
+	printf("Number: %lf", number);
+	printf("\nCharacter: %c", alphabet);
+
+
+	return 0;
+}
+
+
