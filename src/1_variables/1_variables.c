618b9311b3fcda3e4673b7fe6376a5bdc7d8d8c8
larinpaul <www.larinpavel@mail.ru>
2024-05-16 08:01:50 +0500

    User input in C

--- src/1_variables/1_variables.c
+++ src/1_variables/1_variables.c
@@ -1,40 +1,40 @@
-#include <stdio.h>
-
-int main() {
-
-	int age = 25;
-	printf("Age: %d", age);
-
-	age = 31;
-	printf("\nNew age: %d", age);
-
-	int firstNumber = 33;
-	printf("firstNumber = %d", firstNumber);
-
-	int secondNumber = firstNumber;
-	printf("\nsecondNumber = %d", secondNumber);
-
-
-
-	// Multiple variables in a single line
-	int variable1, variable2 = 25;
-	// two variables are created, and the value of variable2 is 25
-	printf("\nVariable1: %d", variable1); // ‘variable1’ is used uninitialized [-Wuninitialized]
-	// If we don't have a value, an unpedictable value could be inserted
-	// however in our compiler, it is zero...
-	printf("\nVariable3: %d", variable2);
-
-	int var1, var2 = 22, var3 = 33;
-	printf("\nVar1: %d", var1); // ‘var1’ is used uninitialized [-Wuninitialized]
-	printf("\nVar2: %d", var2);
-	printf("\nVar3: %d", var3);
-
-
-
-	// Variable naming conventions
-	int myAge, firstNumber, lightSpeed;
-
-
-
-	return 0;
-}
+//#include <stdio.h>
+//
+//int main() {
+//
+//	int age = 25;
+//	printf("Age: %d", age);
+//
+//	age = 31;
+//	printf("\nNew age: %d", age);
+//
+//	int firstNumber = 33;
+//	printf("firstNumber = %d", firstNumber);
+//
+//	int secondNumber = firstNumber;
+//	printf("\nsecondNumber = %d", secondNumber);
+//
+//
+//
+//	// Multiple variables in a single line
+//	int variable1, variable2 = 25;
+//	// two variables are created, and the value of variable2 is 25
+//	printf("\nVariable1: %d", variable1); // ‘variable1’ is used uninitialized [-Wuninitialized]
+//	// If we don't have a value, an unpedictable value could be inserted
+//	// however in our compiler, it is zero...
+//	printf("\nVariable3: %d", variable2);
+//
+//	int var1, var2 = 22, var3 = 33;
+//	printf("\nVar1: %d", var1); // ‘var1’ is used uninitialized [-Wuninitialized]
+//	printf("\nVar2: %d", var2);
+//	printf("\nVar3: %d", var3);
+//
+//
+//
+//	// Variable naming conventions
+////	int myAge1, firstNumber1, lightSpeed1;
+//
+//
+//
+//	return 0;
+//}
