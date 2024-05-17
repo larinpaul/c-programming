c184ba98da9a63798798eeb9966eb868abbea873
larinpaul <www.larinpavel@mail.ru>
2024-05-17 07:35:48 +0500

    Comments in C

--- src/4_user_input/user_input.c
+++ src/4_user_input/user_input.c
@@ -1,44 +1,44 @@
-#include <stdio.h>
-
-int main() {
-
-	// Take int input
-
-//	int age;
+//#include <stdio.h>
 //
-//	printf("Enter input value: ");
-//	scanf("%d", &age); // & denotes memory access
+//int main() {
 //
-//	printf("Age = %d", age);
-
-
-	// Take double and character input:
-
+//	// Take int input
+//
+////	int age;
+////
+////	printf("Enter input value: ");
+////	scanf("%d", &age); // & denotes memory access
+////
+////	printf("Age = %d", age);
+//
+//
+//	// Take double and character input:
+//
+////	double number;
+////	char alphabet;
+////
+////	printf("Enter double input: ");
+////	scanf("%lf", &number);
+////
+////	printf("Enter character input: ");
+////	scanf("\n%c", &alphabet);
+////
+////	printf("Number: %lf", number);
+////	printf("\nCharacter: %c \n", alphabet);
+//
+//
+//	// TAke multiple input
 //	double number;
 //	char alphabet;
 //
-//	printf("Enter double input: ");
-//	scanf("%lf", &number);
-//
-//	printf("Enter character input: ");
-//	scanf("\n%c", &alphabet);
+//	printf("Enter input values: ");
+//	scanf("%lf %c", &number, &alphabet);
 //
 //	printf("Number: %lf", number);
-//	printf("\nCharacter: %c \n", alphabet);
-
-
-	// TAke multiple input
-	double number;
-	char alphabet;
-
-	printf("Enter input values: ");
-	scanf("%lf %c", &number, &alphabet);
-
-	printf("Number: %lf", number);
-	printf("\nCharacter: %c", alphabet);
-
-
-	return 0;
-}
-
-
+//	printf("\nCharacter: %c", alphabet);
+//
+//
+//	return 0;
+//}
+//
+//
---
+++ src/5_comments_in_C/comments_in_c.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main() {
+
+	int data = 34; // create a variable
+
+	printf("Data = %d", data); // print the values of data variables
+
+
+	// Comments can help us debug code...
+
+	// Program to take age and height input
+
+//	int age;
+	double height;
+
+//	printf("Enter the age: ");
+//	scanf("%d", &age);
+
+//	printf("Enter the height: ");
+//	scanf("%lf", &height);
+//
+//	printf("Age = %d", age);
+//	printf("\nHeight = %.1lf", height);
+
+
+	/* This program takes age input from the user
+	 It stores it in the age variable
+	 And, print the value using prinf() */
+
+	// We can also use KEYBOARD SHORTCUTS for multiline comments
+	// Ctrl+Shift+ForwardSlash
+//	This text is to be commented
+	// Okay, in my case it didn't work, but at least I tried...
+
+	int age;
+
+	printf("Enter the age: ");
+	scanf("%d", &age);
+
+	printf("Age = %d", age);
+
+	return 0;
+}
