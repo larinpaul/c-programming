12b7835a49f63d586cb7d9e5ffbee8f78062f567
larinpaul <www.larinpavel@mail.ru>
2024-05-18 09:34:22 +0500

    Operators in C

--- src/6_c_operators/operators.c
+++ src/6_c_operators/operators.c
@@ -1,78 +1,78 @@
-#include <stdio.h>
-//#include <conio.h> // I have to install it separately, I guess?
-
-// int main() is preferred because it conforms
-// to the standard set by the ANSI C and ISO C++ specifications.
-// Using void main() is non-standard and should be avoided
-// as it does not return an exit status
-void main() {
-	printf("Thanks!");
-//	getch();
-
-	int age = 24; // = is an assignment operator
-
-	// Addition			+
-	// Subtraction		-
-	// Multiplication	*
-	// Division			/
-	// Remainder 		%
-	// Increment 		++
-	// DEcrement		--
-
-	int x = 12;
-
-	int result = x + 8;
-
-	printf("\n%d", x + 8); // 20
-
-
-	double xx = 12.57;
-
-	double resultxx = xx + 8.96;
-
-	printf("\n%.2lf", resultxx); // 21.53
-
-
-	// Division
-
-	int xxx = 12;
-
-	int resultxxx = xxx/8; // 1 because of int division...
-
-	printf("\n%d", resultxxx); // 1
-///////////
-	double xxxx = 12.00;
-
-	double resultxxxx = xxxx / 8.00;
-
-	printf("\n%.2lf", resultxxxx); // 1.50
-
-
-	// Remainder operator. It as also called Modulus operator
-	int xxxxx = 12; // Can only be used with integer data. If used with floating number, you will get an error
-
-	int resultxxxxx = xxxxx % 8; // 4
-
-	printf("%d", resultxxxxx);
-
-
-	// Increment operator
-	int x6 = 12;
-
-	printf("\n%d", ++x6); // 13 // prefix increment
-	printf("\n%d", --x6); // 12 // prefix decrement
-//	printf("\n%d", ----x6); // error
-	printf("\n%d", x6--); // 12 // postfix decrement (WON'T PRINT 11 AS EXPECTED)
-	printf("\n%d", x6); // 11 // THIS IS BECAUSE THE PROCEDURE WORKS AFTER THE FUNCTION CALL BASICALLY
-
-
-	// Multiple operators in a single expression
-	int x7 = 4 / 2 + 6 * 5 - 1; // 31 // division, multiplication, addition, subtraction
-	// basically the same as x7 = (4 / 2) + (6 * 5) - 1
-	// This is because of PRECEDENCE and ASSOCIATIVITY
-	// PRECEDENCE dictates the order in which the operators will be evolved in an expression.
-	// ASSOCIATIVITY, on the other hand, defines the order in which the operators of the same precedence will be evaluated in an expression
-
-	printf("\n%d", x7); // 31
-
-}
+//#include <stdio.h>
+////#include <conio.h> // I have to install it separately, I guess?
+//
+//// int main() is preferred because it conforms
+//// to the standard set by the ANSI C and ISO C++ specifications.
+//// Using void main() is non-standard and should be avoided
+//// as it does not return an exit status
+//void main() {
+//	printf("Thanks!");
+////	getch();
+//
+//	int age = 24; // = is an assignment operator
+//
+//	// Addition			+
+//	// Subtraction		-
+//	// Multiplication	*
+//	// Division			/
+//	// Remainder 		%
+//	// Increment 		++
+//	// DEcrement		--
+//
+//	int x = 12;
+//
+//	int result = x + 8;
+//
+//	printf("\n%d", x + 8); // 20
+//
+//
+//	double xx = 12.57;
+//
+//	double resultxx = xx + 8.96;
+//
+//	printf("\n%.2lf", resultxx); // 21.53
+//
+//
+//	// Division
+//
+//	int xxx = 12;
+//
+//	int resultxxx = xxx/8; // 1 because of int division...
+//
+//	printf("\n%d", resultxxx); // 1
+/////////////
+//	double xxxx = 12.00;
+//
+//	double resultxxxx = xxxx / 8.00;
+//
+//	printf("\n%.2lf", resultxxxx); // 1.50
+//
+//
+//	// Remainder operator. It as also called Modulus operator
+//	int xxxxx = 12; // Can only be used with integer data. If used with floating number, you will get an error
+//
+//	int resultxxxxx = xxxxx % 8; // 4
+//
+//	printf("%d", resultxxxxx);
+//
+//
+//	// Increment operator
+//	int x6 = 12;
+//
+//	printf("\n%d", ++x6); // 13 // prefix increment
+//	printf("\n%d", --x6); // 12 // prefix decrement
+////	printf("\n%d", ----x6); // error
+//	printf("\n%d", x6--); // 12 // postfix decrement (WON'T PRINT 11 AS EXPECTED)
+//	printf("\n%d", x6); // 11 // THIS IS BECAUSE THE PROCEDURE WORKS AFTER THE FUNCTION CALL BASICALLY
+//
+//
+//	// Multiple operators in a single expression
+//	int x7 = 4 / 2 + 6 * 5 - 1; // 31 // division, multiplication, addition, subtraction
+//	// basically the same as x7 = (4 / 2) + (6 * 5) - 1
+//	// This is because of PRECEDENCE and ASSOCIATIVITY
+//	// PRECEDENCE dictates the order in which the operators will be evolved in an expression.
+//	// ASSOCIATIVITY, on the other hand, defines the order in which the operators of the same precedence will be evaluated in an expression
+//
+//	printf("\n%d", x7); // 31
+//
+//}
