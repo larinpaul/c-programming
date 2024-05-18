f0b6e26a74e6362e48421f83a32f29b45e9073bf
larinpaul <www.larinpavel@mail.ru>
2024-05-18 08:46:47 +0500

    C Operators

---
+++ src/6_c_operators/operators.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+//#include <conio.h> // I have to install it separately, I guess?
+
+// int main() is preferred because it conforms
+// to the standard set by the ANSI C and ISO C++ specifications.
+// Using void main() is non-standard and should be avoided
+// as it does not return an exit status
+void main() {
+	printf("Thanks!");
+//	getch();
+
+
+
+
+}
+
+
