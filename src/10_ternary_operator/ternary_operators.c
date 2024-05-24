fa1a9d888ab2606b13d1563396044b50ded8178c
larinpaul <www.larinpavel@mail.ru>
2024-05-24 13:20:58 +0500

    10: Ternary Operator in C

---
+++ src/10_ternary_operator/ternary_operators.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+			    //   if true    // if false
+// test_condition ? expression1 : expression2;
+	// Here you can see three operators, hence the name
+		// "ternary operator"
+
+int main() {
+
+	int age = 15;
+
+	(age >= 18) ? printf("You can vote\n") : printf("You cannot vote\n");
+
+
+	// We can also store the value from a ternary operator
+	// into a variable and use it later.
+
+	char operator = '+';
+
+	int num1 = 8;
+	int num2 = 7;
+
+ 	int result = (operator == '+') ? (num1 + num2) : (num1 - num2);
+ 	printf("%d\n", result); // 15
+
+
+ 	// Depending upon your requirement,
+ 	// you can either print values directly
+ 	// or assign a value to a variable from a ternary operator
+ 	// However, if you want to use the value later in a program,
+ 	// it's a good idea to store the value in a variable
+
+
+ 	// Ternary Operator Vs if...else Statement
+
+
+ 	int age21 = 15;
+
+ 	(age21 >= 18) ?
+ 		printf("You can vote") :
+		printf("You cannot vote");
+
+
+ 	int age22 = 15;
+
+ 	if (age >= 18) {
+ 		printf("You can vote");
+ 	}
+
+ 	else {
+ 		printf("You cannot vote");
+ 	}
+
+
+
+	return 0;
+}
