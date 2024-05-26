6f893e645912cf7285af460f14c5e6fe427aad8c
larinpaul <www.larinpavel@mail.ru>
2024-05-26 13:16:34 +0500

    12: While Loop in C

--- src/12_while_loop/while_loop.c
+++ src/12_while_loop/while_loop.c
@@ -1,70 +1,70 @@
-//while (condition) {
-//	// statements inside while
-//}
-
-#include <stdio.h>
-
-int main() {
-
-////	while (1 < 5) { // Infinite loop!
-////		printf("while loop in C\n");
-////	}
+////while (condition) {
+////	// statements inside while
+////}
 //
+//#include <stdio.h>
+//
+//int main() {
+//
+//////	while (1 < 5) { // Infinite loop!
+//////		printf("while loop in C\n");
+//////	}
+////
+//////	int count = 1;
+//////	while (count < 5) {
+//////		printf("while loop in C\n");
+//////		printf("%d\n", count);
+////////		count++;
+//////		count = count + 1;
+//////	}
+//////
+//////	int number;
+//////	printf("Enter the number:");
+//////	scanf("%d", &number);
+//////
+//////	int count1 = 1;
+//////
+//////	while (count1 <= 10) {
+//////		int product = number * count1;
+//////		printf("%d\n", product);
+//////		count1 = count1 + 1;
+//////	}
+////
+////
+////	int number;
+////	printf("Enter the number: ");
+////	scanf("%d", &number);
+////
 ////	int count = 1;
-////	while (count < 5) {
-////		printf("while loop in C\n");
-////		printf("%d\n", count);
-//////		count++;
+////
+////	while (count <= 10) {
+////		int product = number * count;
+////		printf("%d * %d = %d\n", number, count, product);
 ////		count = count + 1;
 ////	}
 ////
-////	int number;
-////	printf("Enter the number:");
-////	scanf("%d", &number);
-////
-////	int count1 = 1;
-////
-////	while (count1 <= 10) {
-////		int product = number * count1;
-////		printf("%d\n", product);
-////		count1 = count1 + 1;
-////	}
 //
-//
-//	int number;
-//	printf("Enter the number: ");
-//	scanf("%d", &number);
+////	// do...while loop
+////	do {
+////		// body of loop
+////	} while (condition);
 //
 //	int count = 1;
 //
-//	while (count <= 10) {
-//		int product = number * count;
-//		printf("%d * %d = %d\n", number, count, product);
-//		count = count + 1;
-//	}
-//
-
-//	// do...while loop
 //	do {
-//		// body of loop
-//	} while (condition);
-
-	int count = 1;
-
-	do {
-		printf("%d\n", count);
-		count = count + 1;
-	} while (count < 5);
-
-	// The code in the do while loop is executed EVEN if the condition is false!
-
-	int countFalse = 666666;
-
-	do {
-		printf("%d\n", countFalse);
-		count = count + 1;
-	} while (count < 5);
-
-	return 0;
-}
-
+//		printf("%d\n", count);
+//		count = count + 1;
+//	} while (count < 5);
+//
+//	// The code in the do while loop is executed EVEN if the condition is false!
+//
+//	int countFalse = 666666;
+//
+//	do {
+//		printf("%d\n", countFalse);
+//		count = count + 1;
+//	} while (count < 5);
+//
+//	return 0;
+//}
+//
