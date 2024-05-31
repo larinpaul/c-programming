f600c6734c32f2614081927517468d25eb3f3f68
larinpaul <www.larinpavel@mail.ru>
2024-05-31 13:15:29 +0500

    17: C Standard Library Functions

--- src/17_c_standard_library_functions/c_standard_library_functions.c
+++ src/17_c_standard_library_functions/c_standard_library_functions.c
@@ -18,6 +18,26 @@
 #include <math.h>
 
 
+// When you pass a constant value, the compiler
+// can perform the calculation at compile time,
+// so it doesn't need to link the math library.
+// When you compile the program, the compiler only checks the declarations, not definitions.
+// If the linker can't find the definition, you'll get an error or undexpected behavior...
+
+// To fix the issues on your machine, try the following:
+// * Make sure you're compiling your program with the `-lm` linker flag to include the math library.
+// * Declare the math library functions explicitly, even though they are already declared in `math.h`.
+// This can help the compiler and linker understand what's going on.
+// * Check your compiler and linker versions to ensure they're compatible with the math library.
+
+
+// Now let's lean about different library functions available
+// inside the ctype.h header file
+// This file provide functions to perform various operations
+// on characters, hence it is known as a character type header file...
+
+#include <ctype.h>
+
 int main() {
 
 	printf("Hello World\n");
@@ -32,11 +52,38 @@ int main() {
 	// It also works at:
 	// https://www.programiz.com/c-programming/online-compiler/
 
+	// We can also use the cbrt() function to compute the cube root
+
+	int num3 = 27;
+//	printf("Cube root %lf", cbrt(num3));
+//	double num33 = cbrt(num3); // this also doesn't work, sadly...
+	double num33 = cbrt(27); // For some reason, this works, using 27 instead of a variable
+	printf("Cube root %lf\n", num33);
+
+
+	int a = 5;
+	int b = 2;
+
+//	double result = pow(a, b); // Again, doesn't work...
+	double result = pow(5, 2); // While this works for some reason...
+	printf("Power: %lf\n", result); // Power: 25.000000
+	printf("Power: %lf\n", pow(5, 2)); // This also works...
+	// I guess the problem is in connecting the header file or something...
+	// Or maybe the compiler machinations are needed...
+
+
+	printf("Let's work with ctype.h \n");
+	char alpha = 'e';
+	printf("%d\n", alpha); // 101
+	printf("%c\n", alpha); // e
+	// Let's first convert this character to upper case
+	char upper = toupper(alpha);
+	printf("%d\n", upper); // 69
+	printf("%c\n", upper); // N
+
+	char lower = tolower(upper);
+	printf("\n%c\n", lower); // e
 
 
 	return 0;
 }
-
-
-
-
