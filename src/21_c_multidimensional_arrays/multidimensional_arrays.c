f82b79b0e44d8b7b27a9a215a09e82960e761deb
larinpaul <www.larinpavel@mail.ru>
2024-06-03 07:49:54 +0500

    20: C Multidimensional Arrays

---
+++ src/21_c_multidimensional_arrays/multidimensional_arrays.c
@@ -0,0 +1,12 @@
+// Arrays consists of elements
+// Multidimensional arrays also consist of elements
+// The only difference is,
+// the elements of multidimensional arrays are also arrays
+
+int main() {
+
+
+
+
+	return 0;
+}
