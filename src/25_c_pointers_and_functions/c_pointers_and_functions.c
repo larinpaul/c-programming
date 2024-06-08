821df613577a4c88a27664c0d2ef55fe7545ca15
larinpaul <www.larinpavel@mail.ru>
2024-06-08 08:51:11 +0500

    25: C Pointers and Functions

--- src/25_c_pointers_and_functions/c_pointers_and_functions.c
+++ src/25_c_pointers_and_functions/c_pointers_and_functions.c
@@ -1,17 +1,34 @@
 #include <stdio.h>
 
-int squareNummber(int number) {
+// Now, we are passing a variable to a function
+int squareNumber(int number) {
 	int square = number * number;
 	return square;
 }
 
+// But we can also pass a pointer variable...
+// int* is a pointer variable which stores the address of the variable
+void findValue(int* num) {
+
+
+	*num = 39;
+}
+
 int main() {
 
-	int number = 25;
+	int number = 21;
 
-	int result = sqaureNumber(number);
+	int result = squareNumber(number);
 
-	printf("Square of %d is %d", number, result);
+	printf("Square of %d is %d\n", number, result);
+
+
+	int number1 = 21;
+
+	findValue(&number1);
+
+	printf("Number: %d\n", number1);
+
 
 
 
